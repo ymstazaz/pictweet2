227b6ff505c45314cfe5fda24715cdcfcb0a3781
ymstazaz <r37a45d@gmail.com>
2024-10-15 18:06:37 +0900

    モデルに保存する際の制御をかける

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,14 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new
   end
 
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  private # ↓プライベートメソッド（条件などの記載）
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+  
 end
