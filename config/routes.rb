f67018ef870bea092dda129098ba2055b6ae2940
ymstazaz <r37a45d@gmail.com>
2024-10-15 18:28:43 +0900

    destroyアクションのルーティングとビューの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create]
+  resources :tweets, only: [:index, :new, :create, :destroy]
   # Defines the root path route ("/")
   # root "articles#index"
 end
