64b63fcb0f90b3c271042aa65de1fcfc1276f5f6
ymstazaz <r37a45d@gmail.com>
2024-10-16 12:05:29 +0900

    updateアクションのルーティングとコントローラーの設定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -15,7 +15,15 @@ class TweetsController < ApplicationController
   def destroy
     tweet = Tweet.find(params[:id])
     # このパラムスidはURLから引っ張ってくるもの。
+    # 元の記事を引用。（引っ張ってくる）のの定型分↑
     tweet.destroy
+    redirect_to root_path
+    # ルートパスは初期画面。よく使う。
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
     redirect_to root_path
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: 'tweets#index'
-  resources :tweets, only: [:index, :new, :create, :destroy, :edit]
+  resources :tweets, only: [:index, :new, :create, :destroy, :edit, :update]
   # Defines the root path route ("/")
   # root "articles#index"
 end
